7613b18d5d7f3663e9e955c5e3b5f1f3dd285e83
André Braga <andreazevedobraga@gmail.com>
2021-03-01 18:36:06 -0300

    Criando a classe posicao

---
+++ Xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Xadrez_console.tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao() { }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+
+    }
+}
